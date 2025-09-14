59302495314af5d0c4ac052f165d5a72e789ada9
Raphael Palma <117413668+raphaelgpalma@users.noreply.github.com>
2025-09-14 13:39:55 -0300

    Create index.js

---
+++ wikipedia_iframe/public/index.js
@@ -0,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+class WikipediaIframePlugin {
+  setup() {
+    return {};
+  }
+
+  start() {
+    return {};
+  }
+
+  stop() {}
+}
+
+const plugin = () => new WikipediaIframePlugin();
+
+module.exports = { plugin };
